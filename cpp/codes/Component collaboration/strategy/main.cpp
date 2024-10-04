35fdf6dbe23c064a53f72b97711422cb6220d5cc
HaleOIC <shinehale730@gmail.com>
2024-10-04 21:17:51 +0200

    push code files

--- cpp/codes/Component collaboration/strategy/main.cpp
+++
@@ -1,9 +0,0 @@
-#include "SalesOrder.hpp"
-
-int main(){
-	// has not been implemented;
-	StrategyFactory* strategyFactory;
-	SalesOrder newone(strategyFactory);
-	newone.CalculateTax();
-	return 0;
-}
